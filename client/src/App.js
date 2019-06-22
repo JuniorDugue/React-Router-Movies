12ea2c2c525e8acf490d5fd20822c02abbe580f1
Junior Dugue <jrdugue@gmail.com>
2019-06-22 14:32:32 -0400

    imported browserrouter and wrapped app in browser app

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+        
       </div>
     );
   }
